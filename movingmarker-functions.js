5652bb129a44e923a86d4b360e36c7f6e5410775
danireload <danireload@gmail.com>
2021-11-11 15:44:36 -0300

    Update movingmarker-functions.js

--- movingmarker-functions.js
+++ movingmarker-functions.js
@@ -124,8 +124,30 @@ function getuserlocation(){
   //map.locate({setView: true}) //, watch: true, maxZoom: 16 timeout: 10000 //Number of milliseconds to wait for a response from geolocation before firing a locationerror event.
   //stopLocate() //
 
-  navigator.geolocation.getCurrentPosition(function(position) {
-  console.log(position);
+  //navigator.geolocation.getCurrentPosition(function(position) {
+  //console.log(position);
+
+  var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+function success(pos) {
+  var crd = pos.coords;
+
+  console.log('Sua posição atual é:');
+  console.log('Latitude : ' + crd.latitude);
+  console.log('Longitude: ' + crd.longitude);
+  console.log('Mais ou menos ' + crd.accuracy + ' metros.');
+};
+
+function error(err) {
+  console.warn('ERROR(' + err.code + '): ' + err.message);
+};
+
+navigator.geolocation.getCurrentPosition(success, error, options);
+
 });
 
   map.on('locationfound', onLocationFound);
