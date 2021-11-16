51f4badd514b42db9a8041c8628b5a41dbadf4eb
danireload <danireload@gmail.com>
2021-11-16 13:04:58 -0300

    Create geojson-path-finder.js

---
+++ geojson-path-finder.js
@@ -0,0 +1,417 @@
+
+function geoJsonReduce(geojson, fn, seed) {
+    if (geojson.type === 'FeatureCollection') {
+        return geojson.features.reduce(function reduceFeatures(a, f) {
+            return geoJsonReduce(f, fn, a);
+        }, seed);
+    } else {
+        return fn(seed, geojson);
+    }
+}
+
+function geoJsonFilterFeatures(geojson, fn) {
+    var features = [];
+    if (geojson.type === 'FeatureCollection') {
+        features = features.concat(geojson.features.filter(fn));
+    }
+
+    return {
+        type: 'FeatureCollection',
+        features: features
+    };
+}
+
+function isLineString(f) {
+    return f.geometry.type === 'LineString';
+}
+
+function topology(geojson, options) {
+    options = options || {};
+    var keyFn = options.keyFn || function defaultKeyFn(c) {
+            return c.join(',');
+        },
+        precision = options.precision || 1e-5;
+
+    var lineStrings = geoJsonFilterFeatures(geojson, isLineString);
+    var explodedLineStrings = turf.explode(lineStrings);
+    var vertices = explodedLineStrings.features.reduce(function buildTopologyVertices(cs, f, i, fs) {
+            var rc = roundCoord(f.geometry.coordinates, precision);
+            cs[keyFn(rc)] = f.geometry.coordinates;
+
+            if (i % 1000 === 0 && options.progress) {
+                options.progress('topo:vertices', i, fs.length);
+            }
+
+            return cs;
+        }, {}),
+        edges = geoJsonReduce(lineStrings, function buildTopologyEdges(es, f, i, fs) {
+            f.geometry.coordinates.forEach(function buildLineStringEdges(c, i, cs) {
+                if (i > 0) {
+                    var k1 = keyFn(roundCoord(cs[i - 1], precision)),
+                        k2 = keyFn(roundCoord(c, precision));
+                    es.push([k1, k2, f.properties]);
+                }
+            });
+
+            if (i % 1000 === 0 && options.progress) {
+                options.progress('topo:edges', i, fs.length);
+            }
+
+            return es;
+        }, []);
+
+    return {
+        vertices: vertices,
+        edges: edges
+    };
+}
+
+//
+
+function roundCoord(c, precision) {
+    return [
+        Math.round(c[0] / precision) * precision,
+        Math.round(c[1] / precision) * precision,
+    ];
+};
+
+//var Queue = require('tinyqueue');
+
+function findNextEnd(prev, v, vertices, ends, vertexCoords, edgeData, trackIncoming, options) {
+    var weight = vertices[prev][v],
+        reverseWeight = vertices[v][prev],
+        coordinates = [],
+        path = [],
+        reducedEdge = options.edgeDataSeed;
+
+    if (options.edgeDataReduceFn) {
+        reducedEdge = options.edgeDataReduceFn(reducedEdge, edgeData[v][prev]);
+    }
+
+    while (!ends[v]) {
+        var edges = vertices[v];
+
+        if (!edges) { break; }
+
+        var next = Object.keys(edges).filter(function notPrevious(k) { return k !== prev; })[0];
+        weight += edges[next];
+
+        if (trackIncoming) {
+            reverseWeight += vertices[next][v];
+
+            if (path.indexOf(v) >= 0) {
+                ends[v] = vertices[v];
+                break;
+            }
+            path.push(v);
+        }
+
+        if (options.edgeDataReduceFn) {
+            reducedEdge = options.edgeDataReduceFn(reducedEdge, edgeData[v][next]);
+        }
+
+        coordinates.push(vertexCoords[v]);
+        prev = v;
+        v = next;
+    }
+
+    return {
+        vertex: v,
+        weight: weight,
+        reverseWeight: reverseWeight,
+        coordinates: coordinates,
+        reducedEdge: reducedEdge
+    };
+}
+
+function compactNode(k, vertices, ends, vertexCoords, edgeData, trackIncoming, options) {
+    options = options || {};
+    var neighbors = vertices[k];
+    return Object.keys(neighbors).reduce(function compactEdge(result, j) {
+        var neighbor = findNextEnd(k, j, vertices, ends, vertexCoords, edgeData, trackIncoming, options);
+        var weight = neighbor.weight;
+        var reverseWeight = neighbor.reverseWeight;
+        if (neighbor.vertex !== k) {
+            if (!result.edges[neighbor.vertex] || result.edges[neighbor.vertex] > weight) {
+                result.edges[neighbor.vertex] = weight;
+                result.coordinates[neighbor.vertex] = [vertexCoords[k]].concat(neighbor.coordinates);
+                result.reducedEdges[neighbor.vertex] = neighbor.reducedEdge;
+            }
+            if (trackIncoming &&
+                !isNaN(reverseWeight) && (!result.incomingEdges[neighbor.vertex] || result.incomingEdges[neighbor.vertex] > reverseWeight)) {
+                result.incomingEdges[neighbor.vertex] = reverseWeight;
+                var coordinates = [vertexCoords[k]].concat(neighbor.coordinates);
+                coordinates.reverse();
+                result.incomingCoordinates[neighbor.vertex] = coordinates;
+            }
+        }
+        return result;
+    }, {edges: {}, incomingEdges: {}, coordinates: {}, incomingCoordinates: {}, reducedEdges: {}});
+}
+
+function compactGraph(vertices, vertexCoords, edgeData, options) {
+    options = options || {};
+    var progress = options.progress;
+    var ends = Object.keys(vertices).reduce(function findEnds(es, k, i, vs) {
+        var vertex = vertices[k];
+        var edges = Object.keys(vertex);
+        var numberEdges = edges.length;
+        var remove;
+
+        if (options.compact !== undefined && !options.compact) {
+            remove = false;
+        } else if (numberEdges === 1) {
+            var other = vertices[edges[0]];
+            remove = !other[k];
+        } else if (numberEdges === 2) {
+            remove = edges.filter(function(n) {
+                return vertices[n][k];
+            }).length === numberEdges;
+        } else {
+            remove = false;
+        }
+
+        if (!remove) {
+            es[k] = vertex;
+        }
+
+        if (i % 1000 === 0 && progress) {
+            progress('compact:ends', i, vs.length);
+        }
+
+        return es;
+    }, {});
+
+    return Object.keys(ends).reduce(function compactEnd(result, k, i, es) {
+        var compacted = compactNode(k, vertices, ends, vertexCoords, edgeData, false, options);
+        result.graph[k] = compacted.edges;
+        result.coordinates[k] = compacted.coordinates;
+
+        if (options.edgeDataReduceFn) {
+            result.reducedEdges[k] = compacted.reducedEdges;
+        }
+
+        if (i % 1000 === 0 && progress) {
+            progress('compact:nodes', i, es.length);
+        }
+
+        return result;
+    }, {graph: {}, coordinates: {}, reducedEdges: {}});
+};
+
+function preprocess(graph, options) {
+    options = options || {};
+    var weightFn = options.weightFn || function defaultWeightFn(a, b) {
+            return turf.distance(turf.point(a), turf.point(b));
+        },
+        topo;
+
+    if (graph.type === 'FeatureCollection') {
+        // Graph is GeoJSON data, create a topology from it
+        topo = topology(graph, options);
+    } else if (graph.edges) {
+        // Graph is a preprocessed topology
+        topo = graph;
+    }
+
+    var graph = topo.edges.reduce(function buildGraph(g, edge, i, es) {
+        var a = edge[0],
+            b = edge[1],
+            props = edge[2],
+            w = weightFn(topo.vertices[a], topo.vertices[b], props),
+            makeEdgeList = function makeEdgeList(node) {
+                if (!g.vertices[node]) {
+                    g.vertices[node] = {};
+                    if (options.edgeDataReduceFn) {
+                        g.edgeData[node] = {};
+                    }
+                }
+            },
+            concatEdge = function concatEdge(startNode, endNode, weight) {
+                var v = g.vertices[startNode];
+                v[endNode] = weight;
+                if (options.edgeDataReduceFn) {
+                    g.edgeData[startNode][endNode] = options.edgeDataReduceFn(options.edgeDataSeed, props);
+                }
+            };
+
+        if (w) {
+            makeEdgeList(a);
+            makeEdgeList(b);
+            if (w instanceof Object) {
+                if (w.forward) {
+                    concatEdge(a, b, w.forward);
+                }
+                if (w.backward) {
+                    concatEdge(b, a, w.backward);
+                }
+            } else {
+                concatEdge(a, b, w);
+                concatEdge(b, a, w);
+            }
+        }
+
+        if (i % 1000 === 0 && options.progress) {
+            options.progress('edgeweights', i,es.length);
+        }
+
+        return g;
+    }, {edgeData: {}, vertices: {}});
+
+    var compact = compactor.compactGraph(graph.vertices, topo.vertices, graph.edgeData, options);
+
+    return {
+        vertices: graph.vertices,
+        edgeData: graph.edgeData,
+        sourceVertices: topo.vertices,
+        compactedVertices: compact.graph,
+        compactedCoordinates: compact.coordinates,
+        compactedEdges: options.edgeDataReduceFn ? compact.reducedEdges : null
+    };
+};
+
+function(graph, start, end) {
+    var costs = {};
+    costs[start] = 0;
+    var initialState = [0, [start], start];
+    var queue = new Queue([initialState], function(a, b) { return a[0] - b[0]; });
+    var explored = {};
+
+    while (queue.length) {
+        var state = queue.pop();
+        var cost = state[0];
+        var node = state[2];
+        if (node === end) {
+            return state.slice(0, 2);
+        }
+
+        var neighbours = graph[node];
+        Object.keys(neighbours).forEach(function(n) {
+            var newCost = cost + neighbours[n];
+            if (!(n in costs) || newCost < costs[n]) {
+                costs[n] = newCost;
+                var newState = [newCost, state[1].concat([n]), n];
+                queue.push(newState);
+            }
+        });
+    }
+
+    return null;
+}
+
+function PathFinder(graph, options) {
+    options = options || {};
+
+    if (!graph.compactedVertices) {
+        graph = preprocess(graph, options);
+    }
+
+    this._graph = graph;
+    this._keyFn = options.keyFn || function(c) {
+        return c.join(',');
+    };
+    this._precision = options.precision || 1e-5;
+    this._options = options;
+
+    if (Object.keys(this._graph.compactedVertices).filter(function(k) { return k !== 'edgeData'; }).length === 0) {
+        throw new Error('Compacted graph contains no forks (topology has no intersections).');
+    }
+}
+
+PathFinder.prototype = {
+    findPath: function(a, b) {
+        var start = this._keyFn(roundCoord(a.geometry.coordinates, this._precision)),
+            finish = this._keyFn(roundCoord(b.geometry.coordinates, this._precision));
+
+        // We can't find a path if start or finish isn't in the
+        // set of non-compacted vertices
+        if (!this._graph.vertices[start] || !this._graph.vertices[finish]) {
+            return null;
+        }
+
+        var phantomStart = this._createPhantom(start);
+        var phantomEnd = this._createPhantom(finish);
+
+        var path = findPath(this._graph.compactedVertices, start, finish);
+
+        if (path) {
+            var weight = path[0];
+            path = path[1];
+            return {
+                path: path.reduce(function buildPath(cs, v, i, vs) {
+                    if (i > 0) {
+                        cs = cs.concat(this._graph.compactedCoordinates[vs[i - 1]][v]);
+                    }
+
+                    return cs;
+                }.bind(this), []).concat([this._graph.sourceVertices[finish]]),
+                weight: weight,
+                edgeDatas: this._graph.compactedEdges
+                    ? path.reduce(function buildEdgeData(eds, v, i, vs) {
+                        if (i > 0) {
+                            eds.push({
+                                reducedEdge: this._graph.compactedEdges[vs[i - 1]][v]
+                            });
+                        }
+
+                        return eds;
+                    }.bind(this), [])
+                    : undefined
+            };
+        } else {
+            return null;
+        }
+
+        this._removePhantom(phantomStart);
+        this._removePhantom(phantomEnd);
+    },
+
+    serialize: function() {
+        return this._graph;
+    },
+
+    _createPhantom: function(n) {
+        if (this._graph.compactedVertices[n]) return null;
+
+        var phantom = compactor.compactNode(n, this._graph.vertices, this._graph.compactedVertices, this._graph.sourceVertices, this._graph.edgeData, true, this._options);
+        this._graph.compactedVertices[n] = phantom.edges;
+        this._graph.compactedCoordinates[n] = phantom.coordinates;
+
+        if (this._graph.compactedEdges) {
+            this._graph.compactedEdges[n] = phantom.reducedEdges;
+        }
+
+        Object.keys(phantom.incomingEdges).forEach(function(neighbor) {
+            this._graph.compactedVertices[neighbor][n] = phantom.incomingEdges[neighbor];
+            this._graph.compactedCoordinates[neighbor][n] = phantom.incomingCoordinates[neighbor];
+            if (this._graph.compactedEdges) {
+                this._graph.compactedEdges[neighbor][n] = phantom.reducedEdges[neighbor];
+            }
+        }.bind(this));
+
+        return n;
+    },
+
+    _removePhantom: function(n) {
+        if (!n) return;
+
+        Object.keys(this._graph.compactedVertices[n]).forEach(function(neighbor) {
+            delete this._graph.compactedVertices[neighbor][n];
+        }.bind(this));
+        Object.keys(this._graph.compactedCoordinates[n]).forEach(function(neighbor) {
+            delete this._graph.compactedCoordinates[neighbor][n];
+        }.bind(this));
+        if (this._graph.compactedEdges) {
+            Object.keys(this._graph.compactedEdges[n]).forEach(function(neighbor) {
+                delete this._graph.compactedEdges[neighbor][n];
+            }.bind(this));
+        }
+
+        delete this._graph.compactedVertices[n];
+        delete this._graph.compactedCoordinates[n];
+
+        if (this._graph.compactedEdges) {
+            delete this._graph.compactedEdges[n];
+        }
+    }
+};
