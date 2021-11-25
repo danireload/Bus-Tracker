e6aa7f86bb349961de03359961cffb0649238c24
danireload <danireload@gmail.com>
2021-11-25 11:16:52 -0300

    Update movingmarker-functions.js

--- movingmarker-functions.js
+++ movingmarker-functions.js
@@ -1197,7 +1197,7 @@ function testnomadapi() {
     "fly_to": "BAR",
     "return_to": "20/02/2022",
     "return_from": "30/02/2022",
-    "nights_on_trip_from": 5
+    "nights_on_trip_from": 5,
     "via": [
       {
         "locations": ["PAR"],
